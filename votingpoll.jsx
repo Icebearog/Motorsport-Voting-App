ddf3efd9ac332693ec70a279d363025fe11e6442
Jaiden <155736555+Icebearog@users.noreply.github.com>
2026-07-01 19:39:27 +0200

    Change API calls to use HTTPS and refactor code
    Updated the voting poll component to use HTTPS for API calls and improved the overall structure.

--- votingpoll.jsx
+++ votingpoll.jsx
@@ -13,7 +13,7 @@ export default function VotingPoll() {
 
   // 1. GET THE LIVE DATA WHEN THE PAGE LOADS
   useEffect(() => {
-    fetch("http://votingbackend-env.eba-rgejkkwe.eu-central-1.elasticbeanstalk.com/")
+    fetch("https://votingbackend-env.eba-rgejkkwe.eu-central-1.elasticbeanstalk.com/")
       .then((res) => res.json())
       .then((data) => {
         setVotes({
@@ -32,7 +32,7 @@ export default function VotingPoll() {
   const handleVote = (id) => { 
     if (voted) return; 
 
-    fetch(`http://votingbackend-env.eba-rgejkkwe.eu-central-1.elasticbeanstalk.com/`, {
+    fetch(`https://votingbackend-env.eba-rgejkkwe.eu-central-1.elasticbeanstalk.com/`, {
       method: "POST"
     })
       .then((res) => res.json())
